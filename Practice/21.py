f81b9fa3aa9a63c4cb5a379242745a4acf34df17
Greenmanpdx <dragonlee2@gmail.com>
2016-07-12 16:32:17 -0700

    Updating with functions

--- Practice/21.py
+++ Practice/21.py
@@ -1,53 +1,59 @@
 """Black Jack"""
 
-print('Black Jack Advice\n Enter a card\n')
-print('Options are: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K or A.')
+def take_cards_from_user():
+    print('Black Jack Advice\n Enter a card\n')
+    print('Options are: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K or A.')
+    card1 = input()
 
-card1 = input()
+    print('What is your second card?')
+    card2 = input()
 
-print('What is your second card?')
-card2 = input()
-ace1 = False
-ace2 = False
-if card1 == 'J' or card1 == 'K' or card1 == 'Q':
-    score1 = 10
-elif card1 == 'A':
-    ace1 = True
-else:
-    score1 = int(card1)
 
-if card2 == 'J' or card2 == 'K' or card2 == 'Q':
-    score2 = 10
-elif card2 == 'A':
-    ace2 = True
-else:
-    score2 = int(card2)
+def evaluate_cards(card1, card2)
+    ace1 = False
+    ace2 = False
+    if card1 == 'J' or card1 == 'K' or card1 == 'Q':
+        score1 = 10
+    elif card1 == 'A':
+        ace1 = True
+    else:
+        score1 = int(card1)
 
-if ace1 is True and ace2 is False:
-    if 5 < score2 < 11:
+    if card2 == 'J' or card2 == 'K' or card2 == 'Q':
+        score2 = 10
+    elif card2 == 'A':
+        ace2 = True
+    else:
+        score2 = int(card2)
+
+    if ace1 is True and ace2 is False:
+        if 5 < score2 < 11:
+            score1 = 11
+        else:
+            score1 = 1
+    if ace2 is True and ace1 is False:
+        if 5 < score1 < 11:
+            score2 = 11
+        else:
+            score2 = 2
+    if ace2 is True and ace1 is True:
         score1 = 11
-    else:
-        score1 = 1
-if ace2 is True and ace1 is False:
-    if 5 < score1 < 11:
-        score2 = 11
-    else:
-        score2 = 2
-if ace2 is True and ace1 is True:
-    score1 = 11
-    score2 = 1
-total = score1 + score2
-print('\nTotal: ', total)
-if total < 17:
-    print('Hit')
-if 17 <= total < 21:
-    print('stay')
-if total == 21:
-    print('Blackjack!')
+        score2 = 1
+    total = score1 + score2
+    return total
 
-print('\nCard 1: ', card1)
-print('Card 2: ', card2)
-print('score1: ', score1)
-print('score2: ', score2)
-print('ace1: ', ace1)
-print('ace2: ', ace2)
+def show_advice(total):
+    print('\nTotal: ', total)
+    if total < 17:
+        print('Hit')
+    if 17 <= total < 21:
+        print('stay')
+    if total == 21:
+        print('Blackjack!')
+    #
+    # print('\nCard 1: ', card1)
+    # print('Card 2: ', card2)
+    # print('score1: ', score1)
+    # print('score2: ', score2)
+    # print('ace1: ', ace1)
+    # print('ace2: ', ace2)
