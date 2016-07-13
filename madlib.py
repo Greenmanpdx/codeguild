908878a1c5e880079475e4c08c915e50dc86605c
Greenmanpdx <dragonlee2@gmail.com>
2016-07-13 10:26:17 -0700

    Updating with functions

--- madlib.py
+++ madlib.py
@@ -14,10 +14,15 @@ def ad_lib(word):
     phrase = input()
     return phrase
 i = 0
-while i < len(word_list):
-    if '{' in word_list[i]:
-        word = ''.join(word_list[i])
-        word_list[i] = ad_lib(word)
-    i += 1
 
-print('Your story: /n', ' '.join(word_list))
+def main():
+    print('Welcome to Sad Libs!')
+    while i < len(word_list):
+        if '{' in word_list[i]:
+            word = ''.join(word_list[i])
+            word_list[i] = ad_lib(word)
+        i += 1
+
+    print('Your story: /n', ' '.join(word_list))
+
+main()
