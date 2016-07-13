6ec6ad3ba8803022f5040bb19c18d1d9390873c6
Greenmanpdx <dragonlee2@gmail.com>
2016-07-13 10:30:09 -0700

    Updating with functions

--- Practice/spelling.py
+++ Practice/spelling.py
@@ -1,22 +1,34 @@
 """spelling"""
 
-print('Does your word follow i before e?')
-print('Enter a word')
-word = input()
+def get_word_from_user():
+    word = input()
+    return word
 
-word_list = list(word)
-i = 0
-while i - 1 < len(word):
-    if word_list[i] == 'i':
-        if word_list[i + 1] == 'e':
-            print(word + ' follows the rule')
-            break
-    elif word_list[i] == 'e':
-        if word_list[i + 1] == 'i':
-            print(word + ' does not follow the rule')
-            break
-    elif i + 1 == len(word):
-        print('Invalid word, must contain ie or ei')
-        break
 
-    i += 1
+def check_rule(word):
+    word_list = list(word)
+    i = 0
+    while i - 1 < len(word):
+        if word_list[i] == 'i':
+            if word_list[i + 1] == 'e':
+                print(word + ' follows the rule')
+                break
+        elif word_list[i] == 'e':
+            if word_list[i + 1] == 'i':
+                print(word + ' does not follow the rule')
+                break
+        elif i + 1 == len(word):
+            print('Invalid word, must contain ie or ei')
+            break
+
+        i += 1
+
+
+def main():
+    print('Does your word follow i before e?')
+    print('Enter a word')
+    word = get_word_from_user()
+    check_rule(word)
+
+
+main()
